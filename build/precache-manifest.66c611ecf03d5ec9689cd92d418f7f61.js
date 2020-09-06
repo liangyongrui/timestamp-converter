dbffa42c38f943df223ff2b46d5149077f93acbe
liangyongrui <liangyongrui@kuaishou.com>
2020-09-06 23:55:00 +0800

    .

---
+++ build/precache-manifest.66c611ecf03d5ec9689cd92d418f7f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7491724e79c24d06a2a6ce900b98e541",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a35ef0665e6804307227",
+    "url": "/static/css/main.083444b3.chunk.css"
+  },
+  {
+    "revision": "aaf5cae7225b3b4aec95",
+    "url": "/static/js/2.3d9e59ab.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3d9e59ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a35ef0665e6804307227",
+    "url": "/static/js/main.1d5046c6.chunk.js"
+  },
+  {
+    "revision": "e992f10e48bc0d90547b",
+    "url": "/static/js/runtime-main.fdeaff25.js"
+  }
+]);
